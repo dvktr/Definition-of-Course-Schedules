a119cc01937f97c3096fcdb78e505c8524016403
Dvktr <victormendes04012004@gmail.com>
2023-12-03 20:31:53 -0300

    restricao de disciplinas com inscrições em comum não podem estar no mesmo slot

--- restrictions/RestrictionsHandler.py
+++ restrictions/RestrictionsHandler.py
@@ -1,2 +1,12 @@
 from z3 import *
 
+def turn_common_inscriptions_pair_minicourses_to_propositional_logic(cx, cy, slots):
+    if slots > 0:
+        firstCourseInFirstSlot = Bool(f"x_{cx}_1")
+        secondCourseInFirstSlot = Bool(f"x_{cy}_1")
+        formula = Not(And(firstCourseInFirstSlot, secondCourseInFirstSlot))
+        for slot in range(2, slots + 1):
+            formula = And(formula, Not(And(Bool(f"x_{cx}_{slot}"), Bool(f"x_{cy}_{slot}"))))
+
+        return formula
+
