1ac0c4038dbcd18197c5a33d0b539cef609cbb5e
Victor Manuel <victor3mcastro@gmail.com>
2023-12-04 15:20:08 -0300

    Separando cursos

--- utils/Reader.py
+++ utils/Reader.py
@@ -2,3 +2,22 @@ def openArchive(archive_name):
     with open(f"inputs/{archive_name}", "r") as archive:
         lines = archive.readlines()
     return lines
+
+def getCourses(archive_name):
+    lines = openArchive(archive_name)
+
+    courses = []
+    for line in lines:
+        if line.startswith('# Minicursos'):
+            continue
+        if line.startswith('#'):
+            break
+        courses.append(line.split(' ')[1].rstrip('\n'))
+
+    qtd_courses = len(courses)
+
+    minicourses = {}
+    for x in range(0, qtd_courses):
+        minicourses[x + 1] = courses[x]
+
+    return minicourses
